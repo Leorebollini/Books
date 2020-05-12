4a897b83bcdbf4a3152ea3bb05064b9b7dc426c7
Maty <rmatiaslorenzo@gmail.com>
2020-05-12 17:24:24 -0300

    compartir boton y cambio de genero a editorial

--- js/index.js
+++ js/index.js
@@ -4,16 +4,17 @@ $(document).ready(function(){
 		var filtro = $("#filtro").val();
 		var search = $("#book").val();
 		var resultado = $("#libreria");
-
+		var link = "compartir.html";
 		$.get("https://www.googleapis.com/books/v1/volumes?q="+filtro+ search, function(response){
 			$.each(response.items, function(index, elemento){	
 				console.log(elemento.volumeInfo);
             	resultado.append(      	
                 '<article class="libro">' +
                    '<div>' +  
-                       '<img src='+ elemento.volumeInfo.imageLinks.smallThumbnail +'></img><br>' +
-                       'Titulo: '+ elemento.volumeInfo.title +'<br> Autores: '+ elemento.volumeInfo.authors+'</strong>' + '<br> Año de lanzamiento: ' + elemento.volumeInfo.publishedDate +
-                   		'<br><a href='+elemento.volumeInfo.infoLink+' target=_blank><button class="button" style="background-color:#F24B41;">Leer</button></a>'+
+                       	'<img src='+ elemento.volumeInfo.imageLinks.smallThumbnail +'></img><br>' +
+                       	'Titulo: '+ elemento.volumeInfo.title +'<br> Autores: '+ elemento.volumeInfo.authors+'</strong>' + '<br> Año de lanzamiento: ' + elemento.volumeInfo.publishedDate +
+						'<br><a href='+elemento.volumeInfo.infoLink+' target=_blank><button class="button" style="background-color:#F24B41;">Comprar</button></a>'+
+						'<br><a href='+link+'><button class="button">Compartir</button></a>' +   
                    '</div>' +
                 '<article>'
             	);
