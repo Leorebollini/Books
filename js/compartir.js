1258c24cc3e3f4a6fdf311a44ada8b53e022b0ef
Leorebollini <leo_rebolini@hotmail.es>
2020-05-13 09:12:05 -0300

    impresion de libros en filas

---
+++ js/compartir.js
@@ -0,0 +1,33 @@
+
+
+/*$(document).ready(function(){
+	$("#form").submit(function(){
+		var libro = $("#libro").val;
+		var autor = $("#autor").val;
+		var emisor = $("#emisor").val;
+		var destino = $("#destino").val;
+		var mensaje = $("#mensaje").val;
+	});*//*
+	(function(){
+        emailjs.init("<YOUR USER ID>");
+     })();
+	var data = {
+    service_id: 'default_service',
+    template_id: 'template_sC6D3u09',
+    template_params: {
+        'username': 'James',
+        'g-recaptcha-response': '03AHJ_ASjnLA214KSNKFJAK12sfKASfehbmfd...'
+    }
+};
+ 
+$.ajax('https://api.emailjs.com/api/v1.0/email/send', {
+	    type: 'POST',
+	    data: JSON.stringify(data),
+	    contentType: 'application/json'
+	}).done(function() {
+	    alert('Your mail is sent!');
+	}).fail(function(error) {
+	    alert('Oops... ' + JSON.stringify(error));
+	});*/
+/*
+});*/
