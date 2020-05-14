b4ae9d02d8e2defbd3bdfd7788f26e555de8b1b2
Leorebollini <leo_rebolini@hotmail.es>
2020-05-14 16:17:41 -0300

    email

--- js/compartir.js
+++ js/compartir.js
@@ -1,33 +1,15 @@
+'use strict'
 
-
-/*$(document).ready(function(){
+$(document).ready(function(){
 	$("#form").submit(function(){
-		var libro = $("#libro").val;
-		var autor = $("#autor").val;
-		var emisor = $("#emisor").val;
-		var destino = $("#destino").val;
-		var mensaje = $("#mensaje").val;
-	});*//*
-	(function(){
-        emailjs.init("<YOUR USER ID>");
-     })();
-	var data = {
-    service_id: 'default_service',
-    template_id: 'template_sC6D3u09',
-    template_params: {
-        'username': 'James',
-        'g-recaptcha-response': '03AHJ_ASjnLA214KSNKFJAK12sfKASfehbmfd...'
-    }
-};
- 
-$.ajax('https://api.emailjs.com/api/v1.0/email/send', {
-	    type: 'POST',
-	    data: JSON.stringify(data),
-	    contentType: 'application/json'
-	}).done(function() {
-	    alert('Your mail is sent!');
-	}).fail(function(error) {
-	    alert('Oops... ' + JSON.stringify(error));
-	});*/
-/*
-});*/
+		var libro = $("#libro").val();
+		var autor = $("#autor").val();
+		var destino = $("#destino").val();
+		var asunto = $("#asunto").val();
+		var mensaje = $("#mensaje").val();
+		var body = "El autor es: " + autor + "\n El libro se llama: " + libro + "\n " + mensaje;
+
+    	document.location.href = "mailto:"+destino+"?subject="+asunto+""
+        + "&body=" + encodeURIComponent(body);	
+	});
+});
