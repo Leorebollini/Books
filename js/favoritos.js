b57c45a7178a714edb6eb16c9ecd851ff60facb1
Maty <rmatiaslorenzo@gmail.com>
2020-05-18 20:11:53 -0300

    eliminar fav y css

--- js/favoritos.js
+++ js/favoritos.js
@@ -3,21 +3,42 @@ $(document).ready(function(){
 	var libros = [];
 	var cont = localStorage.getItem('cont');	
 	while(cont != 0){
-		libros.push(JSON.parse(localStorage.getItem(cont)));
+		if(JSON.parse(localStorage.getItem(cont)) !== null){
+			libros.push(JSON.parse(localStorage.getItem(cont)));
+		}
 		cont -= 1;
 	}
 	console.log(libros);
 	var resultado = $("#libreria");
-	$.each(libros, function(index, elemento){	
+	$.each(libros, function(index, elemento){
 		console.log(elemento);
-    	resultado.append(      	
-        '<article class="libro" >' +
-           '<div>' +  
-               	'<img src='+ elemento.volumeInfo.imageLinks.smallThumbnail +'></img><br>' +
-               	'Titulo: '+ elemento.volumeInfo.title +'<br> Autores: '+ elemento.volumeInfo.authors+'</strong>' + '<br> Año de lanzamiento: ' + elemento.volumeInfo.publishedDate +
-				'<br><a href='+elemento.volumeInfo.infoLink+' target=_blank><button class="button" style="background-color:#F24B41;">Ver</button></a>'+  
-           '</div>' +
-        '<article>'
-    	);
+		if(elemento !== null){
+			resultado.append(      	
+				'<article class="libro" >' +
+				   '<div>' +  
+						   '<img src='+ elemento.volumeInfo.imageLinks.smallThumbnail +'></img><br>' +
+						   'Titulo: '+ elemento.volumeInfo.title +'<br> Autores: '+ elemento.volumeInfo.authors+'</strong>' + '<br> Año de lanzamiento: ' + elemento.volumeInfo.publishedDate +
+						'<br><a href='+elemento.volumeInfo.infoLink+' target=_blank><button class="button" style="background-color:#F24B41;">Comprar</button></a>'+  
+						'<br><button class="favbtn" id='+elemento.id+' onClick="eliminarFavs(this.id)">Eliminar de favoritos</button>' + 
+				   '</div>' +
+				'<article>'
+			);
+		}
     });
 });
+
+function eliminarFavs(id){
+	var libro;
+	var cont = localStorage.getItem('cont');
+	while(cont != 0){
+		libro = JSON.parse(localStorage.getItem(cont));
+		console.log(libro)
+		if(libro !== null){
+			if(libro.id === id){
+				localStorage.removeItem(cont)
+			}
+		}
+		cont -= 1;
+	}
+	window.location.reload(true);
+}
