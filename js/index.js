4dadcc22e7dc41f68622aef2e85a6934c659fc51
Leorebollini <leo_rebolini@hotmail.es>
2020-05-22 18:30:02 -0300

    implementacion de ver mas terminado

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 'use strict'
-var maxresult = 10;
+var maxresult = 15;
 var favorito = [];
+var search;
+var filtro;
 
 function favoritos(elemento){
 	var cond = true;
@@ -26,34 +28,57 @@ function favoritos(elemento){
 		}
 	});
 }
-
+function vertodos(){
+	$("#vertodos").hide();
+	var vertodos = $("#libreria");
+	var link = "compartir.html";
+	$.get("https://www.googleapis.com/books/v1/volumes?maxResults="+19+"&q="+filtro+ search, function(response){
+		$.each(response.items, function(index, elemento){
+			if(index > 10){
+		    	vertodos.append(      	
+		        '<article class="libro" >' +
+		           '<div>' +  
+		               	'<img src='+ elemento.volumeInfo.imageLinks.smallThumbnail +'></img><br>' +
+		               	'Titulo: '+ elemento.volumeInfo.title +'<br> Autores: '+ elemento.volumeInfo.authors+'</strong>' + '<br> Año de lanzamiento: ' + elemento.volumeInfo.publishedDate +
+						'<br><a href='+elemento.volumeInfo.infoLink+' target=_blank><button class="button" style="background-color:#F24B41;">Comprar</button></a>'+
+						'<br><a href='+link+'><button class="button" style="background-color: lightskyblue;">Compartir</button></a>' + 
+						'<br><button class="button" id='+elemento.id+' style="background-color: goldenrod;" onclick="favoritos(this.id)" >Agregar a favoritos</button>' +  
+		           '</div>' +
+		        '<article>'
+		    	);
+			}
+		});
+	});
+}
 $(document).ready(function(){
 	$("#myform").submit(function(){
-		var filtro = $("#filtro").val();
-		var search = $("#book").val();
+		filtro = $("#filtro").val();
+		search = $("#book").val();
 		var resultado = $("#libreria");
 		var link = "compartir.html";
 
 		$.get("https://www.googleapis.com/books/v1/volumes?maxResults="+maxresult+"&q="+filtro+ search, function(response){
 			$.each(response.items, function(index, elemento){	
-            	resultado.append(      	
-                '<article class="libro" >' +
-                   '<div>' +  
-                       	'<img src='+ elemento.volumeInfo.imageLinks.smallThumbnail +'></img><br>' +
-                       	'Titulo: '+ elemento.volumeInfo.title +'<br> Autores: '+ elemento.volumeInfo.authors+'</strong>' + '<br> Año de lanzamiento: ' + elemento.volumeInfo.publishedDate +
-						'<br><a href='+elemento.volumeInfo.infoLink+' target=_blank><button class="button" style="background-color:#F24B41;">Comprar</button></a>'+
-						'<br><a href='+link+'><button class="button" style="background-color: lightskyblue;">Compartir</button></a>' + 
-						'<br><button class="button" id='+elemento.id+' style="background-color: goldenrod;" onclick="favoritos(this.id)" >Agregar a favoritos</button>' +  
-                   '</div>' +
-                '<article>'
-            	);
+				if(index < 10){
+	            	resultado.append(      	
+	                '<article class="libro" >' +
+	                   '<div>' +  
+	                       	'<img src='+ elemento.volumeInfo.imageLinks.smallThumbnail +'></img><br>' +
+	                       	'Titulo: '+ elemento.volumeInfo.title +'<br> Autores: '+ elemento.volumeInfo.authors+'</strong>' + '<br> Año de lanzamiento: ' + elemento.volumeInfo.publishedDate +
+							'<br><a href='+elemento.volumeInfo.infoLink+' target=_blank><button class="button" style="background-color:#F24B41;">Comprar</button></a>'+
+							'<br><a href='+link+'><button class="button" style="background-color: lightskyblue;">Compartir</button></a>' + 
+							'<br><button class="button" id='+elemento.id+' style="background-color: goldenrod;" onclick="favoritos(this.id)" >Agregar a favoritos</button>' +  
+	                   '</div>' +
+	                '<article>'
+	            	);
+            	}
             });
-          /*   Boton ver mas - falta implementación
-            if(response.items.length == 10){
+       //      Boton ver todos
+            if(response.items.length > 10){
             	resultado.append(
-            		'<button onclick="vertodos();">Ver más</button>'
+            		'<button id="vertodos" onclick="vertodos();">Ver más</button>'
             	);
-            }*/
+            }
 		});
 	});
 });
